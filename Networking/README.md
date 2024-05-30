1d8c9eb6db723da290ea174fd57da3b723d434de
nick-alfakuwehfla <github.t6wu9@passmail.net>
2024-05-30 01:13:24 -0400

    Added Networking directory.

---
+++ Networking/README.md
@@ -0,0 +1,26 @@
+# staticip.sh README
+***************
+## What does this do?
+This is a small script to assign a static IP to a network interface using the command line. This script requires superuser privileges. 
+ 
+## How to / Usage
+Entering the command `./staticip.sh` with no options or arguments (or not enough options or arguments) will output a brief 'How to' message. 
+
+Run the script (with sudo) with the desired interface's name, the IP address to be assigned, the netmask desired, and the gateway desired, and once the script has determined that you have superuser privileges it will proceed with assigning the provided values to the interface specified via the /etc/networking/interfaces file. The script should display the options being added to the file to the terminal for review as well as output confirmation that the changes have been completed after restating the networking services atuomatically.
+
+## Setting permissions
+
+In order for the command to be executable, it will need to be given execute permissions via:
+
+`sudo chmod +x staticip.sh`
+OR
+`sudo chmod 755 staticip.sh`
+
+
+## Syntax
+
+Example with minimum required options:
+
+`./staticip.sh INTERFACE IP_ADDRESS NETMASK GATEWAY`
+
+`./staticip.sh eth0 192.168.1.10 255.255.255.0 192.168.1.0`
---
+++ Networking/staticip.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# Script to set a static IP to a specified network interface
+
+# Function to display how to / usage
+howto() {
+	echo "Usage: $0 INTERFACE_NAME IP_ADDRESS NETMASK GATEWAY"
+	echo "Assigns a static IP address to the provided network interface. All arguments are required."
+	exit 1
+}
+
+# Ensure user has superuser privileges
+if [[ $EUID -ne 0 ]]; then
+	echo "You must have superuser privileges to use this command."
+	exit 1
+fi
+
+# Ensure user has provided the necessary arguments for the command to function
+
+if [ $# -ne 4 ];
+	howto
+fi
+
+
+# Storing Provided Variables
+INTERFACE=$1
+IP_ADDRESS=$2
+NETMASK=$3
+GATEWAY=$4
+
+# Create a backup of the current networking config
+sudo cp /etc/network/interfaces /etc/network/interfaces.bak
+
+# Configure the interface as provided, display changes to the terminal
+echo "auto $INTERFACE" | sudo tee -a /etc/network/interfaces
+echo "iface $INTERFACE inet static" | sudo tee -a /etc/network/interfaces
+echo "	address $IP_ADDRESS" | sudo tee -a /etc/network/interfaces
+echo "	netmask $NETMASK" | sudo tee -a /etc/network/interfaces
+echo "	gateway $GATEWAY" | sudo tee -a /etc/network/interfaces
+
+# Restart the service
+sudo systemctl restart networking
+
+# Confirm to the user script is complete
+echo "Network interface $INTERFACE is now set to the static IP of $IP_ADDRESS"
